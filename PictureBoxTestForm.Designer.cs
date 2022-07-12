510c243f5b42cb354f6ef5a0f040c777e4066803
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-07-12 10:10:19 -0600

    Add project files.

---
+++ PictureBoxTestForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace PictureBoxTest
+{
+    partial class PictureBoxTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imagePictureBox = new System.Windows.Forms.PictureBox();
+            this.nextImageButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imagePictureBox
+            // 
+            this.imagePictureBox.Location = new System.Drawing.Point(91, 174);
+            this.imagePictureBox.Name = "imagePictureBox";
+            this.imagePictureBox.Size = new System.Drawing.Size(607, 264);
+            this.imagePictureBox.TabIndex = 0;
+            this.imagePictureBox.TabStop = false;
+            // 
+            // nextImageButton
+            // 
+            this.nextImageButton.Location = new System.Drawing.Point(376, 13);
+            this.nextImageButton.Name = "nextImageButton";
+            this.nextImageButton.Size = new System.Drawing.Size(75, 23);
+            this.nextImageButton.TabIndex = 1;
+            this.nextImageButton.Text = "Next Image";
+            this.nextImageButton.UseVisualStyleBackColor = true;
+            this.nextImageButton.Click += new System.EventHandler(this.nextImageButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nextImageButton);
+            this.Controls.Add(this.imagePictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imagePictureBox;
+        private System.Windows.Forms.Button nextImageButton;
+    }
+}
+
---
+++ PictureBoxTestForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBoxTest
+{
+    public partial class PictureBoxTestForm : Form
+    {
+        private int ImageNumber { get; set; } = -1; // The image to display
+        public PictureBoxTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nextImageButton_Click(object sender, EventArgs e)
+        {
+            ImageNumber = (ImageNumber + 1) % 3;  // Cycles from 0 to 2
+
+            // Retrieve image from resources
+            imagePictureBox.Image = (Image)(Properties.Resources.ResourceManager.GetObject(
+                $"image{ImageNumber}"));
+        }
+    }
+}
