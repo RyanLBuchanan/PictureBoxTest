e75881de0f70d6a3251bed8ad9dec6727b932f12
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-07-12 10:31:53 -0600

    feat: Added tooltips

--- PictureBoxTestForm.Designer.cs
+++ PictureBoxTestForm.Designer.cs
@@ -29,8 +29,12 @@ namespace PictureBoxTest
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.imagePictureBox = new System.Windows.Forms.PictureBox();
             this.nextImageButton = new System.Windows.Forms.Button();
+            this.helpfulToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,20 +53,44 @@ namespace PictureBoxTest
             this.nextImageButton.Size = new System.Drawing.Size(75, 23);
             this.nextImageButton.TabIndex = 1;
             this.nextImageButton.Text = "Next Image";
+            this.helpfulToolTip.SetToolTip(this.nextImageButton, "Click button to advance");
             this.nextImageButton.UseVisualStyleBackColor = true;
             this.nextImageButton.Click += new System.EventHandler(this.nextImageButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "This is a label";
+            this.helpfulToolTip.SetToolTip(this.label1, "First Label");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(627, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "This is another label";
+            this.helpfulToolTip.SetToolTip(this.label2, "Second Label");
             // 
             // PictureBoxTestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.nextImageButton);
             this.Controls.Add(this.imagePictureBox);
             this.Name = "PictureBoxTestForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +98,9 @@ namespace PictureBoxTest
 
         private System.Windows.Forms.PictureBox imagePictureBox;
         private System.Windows.Forms.Button nextImageButton;
+        private System.Windows.Forms.ToolTip helpfulToolTip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
